103699f42f73059b4cd768588f653ccb44318180
andreykurmel <andrey.kurmel@faceit-team.com>
2021-05-19 16:31:30 +0300

    Laravel install

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('import')->group(function () {
+    Route::post('csv_prepare', 'Api\CsvImportController@prepareCsv');
+    Route::post('csv_store', 'Api\CsvImportController@storeCsv');
+});
