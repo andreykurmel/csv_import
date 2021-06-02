22d32b387b49f784f3e6585d293ba8c06c3b1816
andreykurmel <andrey.kurmel@faceit-team.com>
2021-06-02 17:07:14 +0300

    Git testing...1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //\Illuminate\Support\Facades\Cache::put('some', 'about');
+    dd(\Illuminate\Support\Facades\Cache::get('some'));
     return view('welcome');
 });
 
